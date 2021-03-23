4406f54f40049663235a0552a5bab5c026095a6c
RC <ricar100003@gmail.com>
2021-03-23 21:20:40 +0100

    next.config.js

--- next.config.js
+++ next.config.js
@@ -5,8 +5,8 @@ module.exports = withPWA({
   pwa: {
     disable: process.env.NODE_ENV === 'development',
     register: true,
-    scope: '.',
-    sw: 'service-worker.js',
+    scope: '/app',
+    sw: 'sw.js',
   },
 })
 
